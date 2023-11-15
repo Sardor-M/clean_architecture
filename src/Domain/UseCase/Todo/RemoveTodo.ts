4e118f59978d80eb5f87d03f648c90cf686d88d1
Sardor Madaminov <sardor0968@gmail.com>
2023-11-15 23:06:43 +0900

    minor fix

--- src/Domain/UseCase/Todo/RemoveTodo.ts
+++ src/Domain/UseCase/Todo/RemoveTodo.ts
@@ -3,10 +3,8 @@ import {TodoRepository} from "../../Repository/TodoRepository";
 export interface RemoveTodosUseCase {
     invoke: (id: string) => Promise<boolean>;
 }
-
 export class RemoveTodo implements RemoveTodosUseCase {
     private todoRepo: TodoRepository;
-
     constructor(_todoRepo: TodoRepository) {
         this.todoRepo = _todoRepo;
     }
